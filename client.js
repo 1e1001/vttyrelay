5408e7d293d722c021a17a155509fbb13d462bcc
1e1001 <michael@malinov.com>
2021-04-07 15:38:11 -0700

    pain 2

--- client.js
+++ client.js
@@ -7,13 +7,13 @@ const port = 1065;
 const hostIP = process.argv[2]
 
 const client = net.connect(port, hostIP, function() {
-	stdout.write("\x1b[2Jestablished connection");
+	stdout.write("\x1b[2Jestablished connection\n");
 });
 
 client.pipe(stdout);
 stdin.pipe(client);
 
 client.on("end", function() {
-	stdout.write("\x1b[2Jllost connection\n");
+	stdout.write("\x1b[2Jlost connection\n");
 	process.exit(0);
 });
